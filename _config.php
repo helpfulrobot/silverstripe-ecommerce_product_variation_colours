53dde7a86551fcecf3c8c63a76327c277a50abdc
Nicolaas <github@sunnysideup.co.nz>
2012-12-28 03:05:53 +0000

    CREATING 2.4 TAG FOR ecommerce_product_variation_colours
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_product_variation_colours/tags/2.4@3781 950b781c-c544-efab-2f99-90083ee69683

---
+++ _config.php
@@ -0,0 +1,14 @@
+<?php
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+
+// __________________________________ START ECOMMERCE PRODUCT VARIATIONS COLOUR MODULE CONFIG __________________________________
+//MUST SET
+//Object::add_extension("ProductAttributeValue", "ProductAttributeDecoratorColour_Value");
+//Object::add_extension("ProductAttributeType", "ProductAttributeDecoratorColour_Type");
+//MAY SET
+//ProductAttributeDecoratorColour_Value::set_default_contrast_colour("FFFFFF");
+//ProductAttributeDecoratorColour_Value::set_default_colour("000000");
+//ProductAttributeDecoratorColour_Value::set_put_styling_in_dropdown_options(true);
+//ColourDropdownField::set_js_location_for_select_styling("ecommerce_product_variation_colours/javascript/ColourDropdownField.js");
+// __________________________________ END ECOMMERCE PRODUCT VARIATIONS COLOUR MODULE CONFIG __________________________________
